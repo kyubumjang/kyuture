b50744e892303ac74ecdd54a274f76651cc20c71
Lawrene Jang <jkb2221@gmail.com>
2024-06-12 11:30:40 +0900

    🐛 fix: modify project name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Alert } from "flowbite-react";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>함께가치 홈페이지</div>
+      <div>KYUTURE</div>
     </main>
   );
 }
